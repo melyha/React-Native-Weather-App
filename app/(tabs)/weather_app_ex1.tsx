5fc8bfd53a447df48eb9d134c7437a5c6c3322e5
melxcreative <melyhaaly@gmail.com>
2026-01-29 15:31:04 -0600

    Add React Native Picker package and implement basic picker component

--- app/(tabs)/weather_app_ex1.tsx
+++ app/(tabs)/weather_app_ex1.tsx
@@ -1,12 +1,30 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { View, Text, StyleSheet, Button } from 'react-native';
 import { Picker } from '@react-native-picker/picker';
 
 const WeatherApp = () => {
+  const [selectedValue, setSelectedValue] = useState('apple');
+
+
   return (
     <View style={styles.container}>
       <Text style={styles.text}>Weather App</Text>
       <Button title='Toggle to '></Button>
+
+ <View style={{ padding: 20 }}>
+      <Picker
+        selectedValue={selectedValue}
+        onValueChange={(itemValue) => setSelectedValue(itemValue)}
+      >
+        <Picker.Item label="Apple" value="apple" />
+        <Picker.Item label="Banana" value="banana" />
+        <Picker.Item label="Orange" value="orange" />
+      </Picker>
+      <Text style={{ marginTop: 10 }}>Selected: {selectedValue}</Text>
+    </View>
+
+
+
     </View>
   );
 };
